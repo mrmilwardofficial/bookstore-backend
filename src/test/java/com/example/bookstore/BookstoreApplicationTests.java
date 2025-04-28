7ea5caa7c64ff5ba64d7791c3885238b0d866e1d
Thompson <thompsonjacketmilward@gmail.com>
2025-04-28 12:09:50 +0200

    Prepare app for Railway deployment

--- src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
